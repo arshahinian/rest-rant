59f863c6af58d0ccc98120102cfe1284c64f6050
Araz Shahinian <arshahinian@gmail.com>
2022-02-22 21:13:00 -0500

    added css and a image for the home page

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,15 @@ function home () {
       <MasterDefault>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img className="img-home" src="/images/deryn-macey-IooxyWj2uso-unsplash_small.jpg" alt="food"/>
+                  <div>
+                    Photo by <a href="https://unsplash.com/@runningonrealfood?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Deryn Macey</a> on <a href="https://unsplash.com/photos/MsTOg6rhRVk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                  </div>
+              </div>
+              <a href="/places">
+                <button className="btn-primary">Places Page</button>
+              </a>
           </main>
       </MasterDefault>
     )
