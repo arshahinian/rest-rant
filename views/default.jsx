482743cffe9107c4ff3c1d121372c42916e05823
Araz Shahinian <arshahinian@gmail.com>
2022-02-26 21:47:57 -0500

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -9,6 +9,19 @@ function masterDefault (html) {
                 <link rel="stylesheet" href="/styles/main.css"/>
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/places">Places</a>
+                        </li>
+                        <li>
+                            <a href="/places/new">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
         </html>
