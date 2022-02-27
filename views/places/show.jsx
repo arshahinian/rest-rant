bd925ba5419a9c9c183d68e9cc9cccd4b0f24f73
Araz Shahinian <arshahinian@gmail.com>
2022-02-27 08:26:03 -0500

    show was fleshed out

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,28 @@
 const React = require('react')
 const MasterDefault = require('../default')
 
-function show () {
+function show (place) {
     return (
         <MasterDefault>
           <main>
-            <h1>Show Page</h1>
+            <h1>{place.name}</h1>
+            <div>
+              <p className="text-center">
+                {place.cuisines}
+              </p>
+              <p className="text-center">
+                <img src={place.pic} alt={place.name}/>
+              </p>
+              <p className="text-center">
+                Located in {place.city}, {place.state}
+              </p>
+            </div>
+            <div>
+              <h2>Comments</h2>
+              <p>
+                Coming Soon!
+              </p>
+            </div>
           </main>
         </MasterDefault>
     )
