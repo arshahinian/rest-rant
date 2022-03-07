2609d972e78336a3e6b7ed6731b0c6978d328ab2
Araz Shahinian <arshahinian@gmail.com>
2022-03-06 19:46:28 -0500

    Activity (6.8.8) - Part Two: Create and Use a Helper Method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,33 +5,33 @@ function show (props) {
     return (
         <MasterDefault>
           <main>
-            <h1>{props.place.name}</h1>
-            <div>
-              <p className="text-center">
-                {props.place.cuisines}
-              </p>
-              <p className="text-center">
-                <img src={props.place.pic} alt={props.place.name}/>
-              </p>
-              <p className="text-center">
-                Located in {props.place.city}, {props.place.state}
-              </p>
-            </div>
-            <div>
-              <h2>Comments</h2>
-              <p>
-                Coming Soon!
-              </p>
-            </div>
-            <div>
-              <a href={`/places/${props.id}/edit`} className="btn btn-warning"> 
-                Edit
-              </a>  
-              <form method="POST" action={`/places/${props.id}?_method=DELETE`}> 
-              <button type="submit" className="btn btn-danger">
-                Delete
-              </button>
-              </form>
+            <div className="row">
+              <div className="col-sm-6">
+                <br/>
+                <img src={props.place.pic} alt={props.place.name} />
+                <br/>
+                <h3>Proud to be located in {props.place.city}, {props.place.state}</h3>
+                <br/>
+              </div>
+              <div className="col-sm-6">
+                <br/>
+                <h1>{props.place.name}</h1>
+                <br/>               
+                <h2>Description</h2>
+                <h3>{props.place.showEstablished()}</h3>
+                <h4>Serving {props.place.cuisines}</h4>
+                <br/>      
+                <h2>Comments</h2>
+                <p>Coming Soon!</p>
+                <br/>
+                <p>
+                  <a href={`/places/${props.id}/edit`} className="btn btn-warning">Edit</a>  
+                </p>            
+                <form method="POST" action={`/places/${props.id}?_method=DELETE`}> 
+                  <button type="submit" className="btn btn-danger">Delete</button>
+                </form>
+                <br/>
+              </div>            
             </div>
           </main>
         </MasterDefault>
